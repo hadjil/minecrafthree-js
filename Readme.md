218d540024fe1e1ae168285484b2f0b226c47a02
Hadji <hadji.tiara@gmail.com>
2025-11-27 22:28:32 -0600

    Primer Commit del Proyecto
    Creación de Escena simple en three.js

---
+++ Readme.md
@@ -0,0 +1,4 @@
+npm create vite@latest
+npm install --save three
+
+![alt text](image.png)
---
+++ src/main.js
@@ -0,0 +1,30 @@
+import * as THREE from 'three'
+
+///REnder Setup
+const renderer = new THREE.WebGLRenderer();
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth,window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+
+//Camera Setup
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight);
+camera.position.set(2,2,2);
+camera.lookAt(0,0,0);
+
+//Scene Setup
+const scene = new THREE.Scene();
+const geometry=new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({color: 0x7FFFD4})
+const cube = new THREE.Mesh(geometry,material);
+scene.add(cube)
+
+//Render Loop
+function animate(){
+  requestAnimationFrame(animate);
+  cube.rotation.x+=0.01;
+  cube.rotation.y=0.01;
+  renderer.render(scene,camera);
+}
+
+animate();
